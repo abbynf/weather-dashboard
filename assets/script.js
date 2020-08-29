c01c99b2fa2e6541cb0ace66e6bb6ae30fa94652
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-08-28 20:39:11 -0600

    added JS to fill text of today forecast with API response

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,22 @@ var selectedCity;
 var modifiedCity;
 var todayForecastCity;
 var todayForecastUrl;
+var enteredCity;
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+
+today = mm+'/'+dd+'/'+yyyy;
+console.log(today);
 
 function callTodayForecast(queryUrl){
 $.ajax({
     url: queryUrl,
     method: 'GET'
 }).then(function(response){
-    console.log(response)
+    fillTodayForecast(response);
 })}
 
 
@@ -20,7 +29,7 @@ $("#go").on("click", callAPIs)
 
 
 function callAPIs(){
-    var enteredCity = $("#typedCity").val()
+    enteredCity = $("#typedCity").val();
     var modifiedCity = "";
     for (i=0; i<enteredCity.length; i++){
         if (enteredCity[i] == " "){
@@ -28,10 +37,17 @@ function callAPIs(){
         } else {
         modifiedCity += enteredCity[i] 
     }}
-    console.log(modifiedCity)
     todayForecastCity = "q=" + modifiedCity;
-    console.log(todayForecastCity)
     todayForecastUrl = "https://api.openweathermap.org/data/2.5/weather?" + todayForecastCity + "&appid=d8733e2675e82a2b5d5803974ae39653"
-    console.log(todayForecastUrl);
     callTodayForecast(todayForecastUrl);
+}
+
+function fillTodayForecast(response){
+    $("#displayCitySearched").text(enteredCity + " " + today);
+    var todayTempF = response.main.temp;
+    var todayTempK = Math.round(((todayTempF - 273.15) * (9/5) + 32)*10) / 10;
+    $("#todayTemp").text("Temperature: " + todayTempK + " F" + String.fromCharCode(176))
+    console.log(response);
+    $("#todayHum").text("Humidity: " + response.main.humidity + String.fromCharCode(37))
+    $("#todayWS").text("Wind speed: " + response.wind.speed + " MPH");
 }
