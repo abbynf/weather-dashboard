69328fa558f79be15d3e4e1ec698500167afdff0
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-08-29 18:16:18 -0600

    added icons to forecasts

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@ console.log("hello")
 var selectedCity;
 var modifiedCity;
 var todayForecastCity;
-var todayForecastUrl;
 var enteredCity;
 var long;
 var latit;
@@ -48,26 +47,41 @@ function callFutureForecast(){
 function fillFutureForecast(response){
     // first future forecast day
     $("#tomorrowDate").text("today plus one");
+    var futIcon1 = response.daily[1].weather[0].icon
+    var futIconUrl1 = "http://openweathermap.org/img/wn/" + futIcon1 +"@2x.png";
+    $("#firstFutureIcon").attr("src", futIconUrl1);
     $("#tomorrow1Temp").text("Temp: " + tempConvert(response.daily[1].temp.day));
     $("#firstFutureHum").text("Humidity: " + response.daily[1].humidity + String.fromCharCode(37))
 
     // Second future forecast day
     // Use moment.js to display the date
+    var futIcon2 = response.daily[2].weather[0].icon
+    var futIconUrl2 = "http://openweathermap.org/img/wn/" + futIcon2 +"@2x.png";
+    $("#secondFutureIcon").attr("src", futIconUrl2);
     $("#secondFutureTemp").text("Temp: " + tempConvert(response.daily[2].temp.day));
     $("#secondFutureHum").text("Humidity: " + response.daily[2].humidity + String.fromCharCode(37));
 
     // Third future forecast day
     // Use moment.js to display date
+    var futIcon3 = response.daily[3].weather[0].icon
+    var futIconUrl3 = "http://openweathermap.org/img/wn/" + futIcon3 +"@2x.png";
+    $("#thirdFutureIcon").attr("src", futIconUrl3);
     $("#thirdFutureTemp").text("Temp: " + tempConvert(response.daily[3].temp.day));
     $("#thirdFutureHum").text("Humidity: " + response.daily[3].humidity + String.fromCharCode(37));
 
     // Fourth future forecast day
     // Use moment.js to display date
+    var futIcon4 = response.daily[4].weather[0].icon
+    var futIconUrl4 = "http://openweathermap.org/img/wn/" + futIcon4 +"@2x.png";
+    $("#fourthFutureIcon").attr("src", futIconUrl4);
     $("#fourthFutureTemp").text("Temp: " + tempConvert(response.daily[4].temp.day));
     $("#fourthFutureHum").text("Humidity: " + response.daily[4].humidity + String.fromCharCode(37));
 
     // Fifth future forecast day
     // Use moment.js to display date
+    var futIcon5 = response.daily[5].weather[0].icon;
+    var futIconUrl5 = "http://openweathermap.org/img/wn/" + futIcon5 +"@2x.png";
+    $("#fifthFutureIcon").attr("src", futIconUrl5);
     $("#fifthFutureTemp").text("Temp: " + tempConvert(response.daily[5].temp.day));
     $("#fifthFutureHum").text("Humidity: " + response.daily[5].humidity + String.fromCharCode(37));
 }
@@ -98,6 +112,9 @@ function fillTodayForecast(response){
     $("#todayWS").text("Wind speed: " + response.wind.speed + " MPH");
     latit = response.coord.lat;
     long = response.coord.lon;
+    var newIcon = response.weather[0].icon;
+    var newIconAddress = "http://openweathermap.org/img/wn/" + newIcon +"@2x.png"
+    $("#todayIcon").attr("src", newIconAddress);
     callUVI();
     callFutureForecast();
 }
